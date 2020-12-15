8c2ac42476b22524d2eace351f6b91afabd89ca8
southl <gyrii84@gmail.com>
2020-12-15 13:38:45 +0200

    add formated date using strftime

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ puts '-'*22
 
 puts "What year were you born?"
 
-year = STDIN.gets.chomp
+year = STDIN.gets.to_i
 
 puts "What number month were you born?"
 
@@ -19,9 +19,17 @@ date_of_month = STDIN.gets.chomp
 
 date = Time.new(year, month, date_of_month)
 
-puts date
+puts '-'*22
+puts date.strftime("You were born on a %A")
+puts date.strftime("It was the %V week of the year.")
+puts date.strftime("It was the %j day of the year.")
 
-# puts "You where born on a #{}"
+if Date.new(year).leap?
+  puts "You were born on a leap year."
+else
+  puts "You were born in a not leap year."
+end
+puts '-'*22
 
 
 
